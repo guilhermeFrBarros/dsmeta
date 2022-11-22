4503bf7461fb75f45dd6a5669c57b5646347d5aa
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2022-11-22 16:00:07 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,10 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +17,15 @@ public class SaleService {
 
     @Autowired
     private SaleRepository repository;
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable){
 
-    public List<Sale> findSales(){
-        return repository.findAll();
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        //serve para criar uma data com o di de hoje ->  serve para pegar o momento atula, pega o fuso horario do sistema
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
